585d1dadf3ce393a47bd843a1c37c9d47dff3cdf
Ivan <ioan.af@mail.ru>
2021-05-15 20:22:54 +0300

    lesson_14

--- jsPrartic.js
+++ jsPrartic.js
@@ -17,18 +17,43 @@
 
 // (nam === 50) ? console.log('ok'): console.log('error');
 
-const nam = 50;
-switch (nam) {
-    case 49:
-        console.log('неверно');
-        break;
-    case 100:
-        console.log('неверно');
-        break;
-    case 50:
-        console.log('ok');
-        break;
-    default:
-        console.log('не в этот раз');
-        break;
+// const nam = 50;
+// switch (nam) {
+//     case 49:
+//         console.log('неверно');
+//         break;
+//     case 100:
+//         console.log('неверно');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default:
+//         console.log('не в этот раз');
+//         break;
+// }
+
+
+// let num = 50;
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+let num = 50;
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+
 }
