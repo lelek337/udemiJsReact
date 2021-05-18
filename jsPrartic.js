e1021c560f4d50cdac2ee5d1db548a704f43903f
Ivan <ioan.af@mail.ru>
2021-05-18 21:16:50 +0300

    lesson_17

--- jsPrartic.js
+++ jsPrartic.js
@@ -55,5 +55,67 @@
 //         continue;
 //     }
 //     console.log(i);
-
 // }
+
+// let num = 20;
+
+// function showFirstMessenge(text) {
+//     console.log(text);
+//     let num = 10;
+// }
+
+// showFirstMessenge('Hello Word');
+// console.log(num);
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 4));
+// console.log(calc(8, 13));
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// function ret() {
+//     let num = 50;
+
+
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('Hello')
+// };
+
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// }
+
+//const str = 'teSt';
+//const arr = [1, 2, 4];
+
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+
+//console.log(str[3]);
+//console.log(str);
+
+//const fruit = 'Some fruit';
+
+//console.log(fruit.indexOf("q"));
+
+//const logg = 'Hello world';
+
+//console.log(logg.slice(-5, -1));
+//console.log(logg.substring(6, 11));
+//console.log(logg.substr(6, 5));
+
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+const test = '12.2';
+console.log(parseInt(test));
