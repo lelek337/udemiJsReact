5937964360082548a5b978bbcd021667d580ae05
Ivan <ioan.af@mail.ru>
2021-05-19 08:25:30 +0300

    practic_3

--- js/script.js
+++ js/script.js
@@ -60,7 +60,63 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '10');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '10');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if (a != null & b != null & a != '' & b != '' & a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('don');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count <= 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count > 10 & personalMovieDB.count <= 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count > 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log("Произошла ошибка");
+// }
+
+// console.log(personalMovieDB);
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+let numberOfFilms;
+
+function start() {
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -70,26 +126,53 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-    if (a != null & b != null & a != '' & b != '' & a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('don');
-    } else {
-        console.log('error');
-        i--;
+
+
+function remembaMyFilm() {
+
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        if (a != null & b != null & a != '' & b != '' & a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('don');
+        } else {
+            console.log('error');
+            i--;
+        }
     }
 }
+remembaMyFilm();
 
-if (personalMovieDB.count <= 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count > 10 & personalMovieDB.count <= 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function detectPersonalLevel() {
+
+    if (personalMovieDB.count <= 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count > 10 & personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
+detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+
+    for (let i = 1; i <= 3; i++) {
+
+        // personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${ i }`);
+        const genre = prompt(`Ваш любимый жанр под номером ${ i }`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+writeYourGenres();
